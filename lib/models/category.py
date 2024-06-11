3c017397650c122d042a2f5157357ce742a1be12
humphrey <writernic89@gmail.com>
2024-06-11 18:49:32 +0300

    ci category.py

--- lib/models/category.py
+++ lib/models/category.py
@@ -1,24 +1,23 @@
 from lib.models.base_model import BaseModel
-
-class Category(BaseModel):
-    def __init__(self, name):
+class Category (BaseModel):
+    def __init__(self, name, id=None):
+        self.id = id
         self.name = name
-
     @classmethod
     def create(cls, name):
-        cls.execute_query("INSERT INTO category (name) VALUES (?)", (name,))
+        if not name:
+            raise ValueError("Category name cannot be empty")
+        cls.execute_query("INSERT INTO category (name) VALUES (?)",(name,))
         return cls(name)
-
     @classmethod
     def delete(cls, category_id):
-        cls.execute_query("DELETE FROM category WHERE id=?", (category_id,))
-
+        cls.execute_query("DELETE FROM category WHERE id=?",(category_id,))
     @classmethod
     def get_all(cls):
         results = cls.fetch_query("SELECT * FROM category")
-        return [cls(result['name']) for result in results]
-
+        return [cls(result[1], result[0]) for result in results]
     @classmethod
     def find_by_id(cls, category_id):
-        result = cls.fetch_query("SELECT * FROM category WHERE id=?", (category_id,))
-        return cls(result[0]['name']) if result else None
+        result = cls.fetch_query ("SELECT * FROM category WHERE id=?", (category_id,))
+        return cls(result[0][1], result[0][0]) if result else None
+        
