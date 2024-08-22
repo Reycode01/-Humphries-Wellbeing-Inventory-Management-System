9b8da8d0621e523b7a3e8cb6825b20c3a7f9f640
humphrey <writernic89@gmail.com>
2024-08-22 15:29:46 +0300

    funct:PS1 variable

--- lib/update_schema.py
+++ lib/update_schema.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def add_price_column():
-    connection = sqlite3.connect('your_database_file.db')  # Replace with your actual database file
+    connection = sqlite3.connect('your_database_file.db') 
     cursor = connection.cursor()
     cursor.execute("ALTER TABLE product ADD COLUMN price REAL")
     connection.commit()
